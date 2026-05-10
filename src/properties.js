bdbb37b6d92dd4574176b6910712840f69d3f6f7
nikitaravojt <92792861+nikitaravojt@users.noreply.github.com>
2026-05-10 23:21:39 +0100

    Update properties.js

--- src/properties.js
+++ src/properties.js
@@ -2,7 +2,7 @@
  * Name as used as title for the main page
  * @type {string}
  */
-var title = 'SurVis Literature Collection';
+title: 'LLM Jailbreak Literature Collection';
 
 /**
  * Relative path to the data directory
