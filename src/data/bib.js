5471a646b1b70a72e9213fe60ab056a4f6eda476
nikitaravojt <92792861+nikitaravojt@users.noreply.github.com>
2026-05-10 18:13:12 +0100

    Update bib.js

--- src/data/bib.js
+++ src/data/bib.js
@@ -4,108 +4,117 @@ define({ entries : {
         "author": "Yi, Sibo and Liu, Yule and Sun, Zhen and Cong, Tianshuo and He, Xinlei and Song, Jiaxing and Xu, Ke and Li, Qi",
         "doi": "10.48550/arXiv.2407.04295",
         "journal": "arXiv preprint arXiv:2407.04295",
-        "keywords": "landscape, survey, taxonomy, attack, defense",
+        "keywords": "type:landscape, survey, attack, defense, evaluation, ASR",
         "title": "Jailbreak Attacks and Defenses Against Large Language Models: A Survey",
         "type": "article",
         "year": "2024"
     },
+
     "Zou2023Universal": {
         "abstract": "",
         "author": "Zou, Andy and Wang, Zifan and Carlini, Nicholas and Nasr, Milad and Kolter, J. Zico and Fredrikson, Matt",
         "doi": "10.48550/arXiv.2307.15043",
         "journal": "arXiv preprint arXiv:2307.15043",
-        "keywords": "landscape, attack, GCG, adversarial-suffix, gradient-based",
+        "keywords": "type:landscape, attack, white_box, GCG, AdvBench, ASR",
         "title": "Universal and Transferable Adversarial Attacks on Aligned Language Models",
         "type": "article",
         "year": "2023"
     },
+
     "Chao2024JailbreakBench": {
         "abstract": "",
         "author": "Chao, Patrick and Debenedetti, Edoardo and Robey, Alexander and Andriushchenko, Maksym and Croce, Francesco and Sehwag, Vikash and Dobriban, Edgar and Flammarion, Nicolas and Pappas, George J. and Tramer, Florian and Hassani, Hamed and Wong, Eric",
         "booktitle": "Advances in Neural Information Processing Systems",
         "doi": "10.52202/079017-1745",
-        "keywords": "landscape, benchmark, evaluation, ASR",
+        "keywords": "type:landscape, benchmark, evaluation, JailbreakBench, ASR, attack, defense",
         "title": "JailbreakBench: An Open Robustness Benchmark for Jailbreaking Large Language Models",
         "type": "inproceedings",
         "volume": "37",
         "year": "2024"
     },
+
     "Arditi2024Refusal": {
         "abstract": "",
         "author": "Arditi, Andy and Obeso, Oscar and Syed, Aaquib and Paleka, Daniel and Panickssery, Nina and Gurnee, Wes and Nanda, Neel",
         "booktitle": "Advances in Neural Information Processing Systems",
         "doi": "10.52202/079017-4322",
-        "keywords": "internal-safety, refusal-direction, activation-space, mechanistic-interpretability",
+        "keywords": "type:internal_safety, refusal, representation, refusal_direction, mechanistic_interpretability, activation_intervention, white_box, GCG",
         "pages": "136037--136083",
         "title": "Refusal in Language Models Is Mediated by a Single Direction",
         "type": "inproceedings",
         "volume": "37",
         "year": "2024"
     },
+
     "Zhao2025Harmfulness": {
         "abstract": "",
         "author": "Zhao, Jiachen and Huang, Jing and Wu, Zhengxuan and Bau, David and Shi, Weiyan",
         "booktitle": "Advances in Neural Information Processing Systems",
         "doi": "10.48550/arXiv.2507.11878",
-        "keywords": "internal-safety, harmfulness, refusal-direction, representation",
+        "keywords": "type:internal_safety, refusal, harmfulness, harmfulness_representation, representation, refusal_direction, ASR",
         "title": "LLMs Encode Harmfulness and Refusal Separately",
         "type": "inproceedings",
         "volume": "38",
         "year": "2025"
     },
+
     "Zhao2026SafetyNeurons": {
         "abstract": "",
         "author": "Zhao, Chongwen and Ke, Yutong and Huang, Kaizhu",
         "booktitle": "Proceedings of the 19th Conference of the European Chapter of the Association for Computational Linguistics (Volume 1: Long Papers)",
         "doi": "10.18653/v1/2026.eacl-long.83",
-        "keywords": "internal-safety, safety-neurons, MLP, defense",
+        "keywords": "type:internal_safety, refusal, harmfulness, representation, safety_neurons, defense, JailbreakBench, ASR",
         "pages": "1889--1906",
         "publisher": "Association for Computational Linguistics",
         "title": "Unraveling LLM Jailbreaks Through Safety Knowledge Neurons",
         "type": "inproceedings",
         "year": "2026"
     },
+
     "Xu2024SCAV": {
         "abstract": "",
         "author": "Xu, Zhihao and Huang, Ruixuan and Chen, Changyu and Wang, Xiting",
         "booktitle": "Advances in Neural Information Processing Systems",
         "doi": "10.52202/079017-3706",
-        "keywords": "exploitation, concept-activation-vector, safety-boundary, attack",
+        "keywords": "type:jailbreak_mechanisms, attack, representation, concept_activation_vector, safety_boundary, AdvBench, ASR, white_box",
         "title": "Uncovering Safety Risks of Large Language Models through Concept Activation Vector",
         "type": "inproceedings",
         "volume": "37",
         "year": "2024"
     },
+
     "Gao2025SafetyBoundaries": {
         "abstract": "",
         "author": "Gao, Lang and Geng, Jiahui and Zhang, Xiangliang and Nakov, Preslav and Chen, Xiuying",
         "booktitle": "Proceedings of the 63rd Annual Meeting of the Association for Computational Linguistics (Volume 1: Long Papers)",
         "doi": "10.18653/v1/2025.acl-long.1233",
-        "keywords": "exploitation, safety-boundary, activation-shift, defense",
+        "keywords": "type:jailbreak_mechanisms, defense, representation, safety_boundary, activation_intervention, AdvBench, ASR",
         "pages": "25378--25398",
         "publisher": "Association for Computational Linguistics",
         "title": "Shaping the Safety Boundaries: Understanding and Defending Against Jailbreaks in Large Language Models",
         "type": "inproceedings",
         "year": "2025"
     },
+
     "Yeo2025SAERefusal": {
         "abstract": "",
         "author": "Yeo, Wei Jie and Prakash, Nirmalendu and Neo, Clement and Satapathy, Ranjan and Lee, Roy Ka-Wei and Cambria, Erik",
         "booktitle": "Findings of the Association for Computational Linguistics: EMNLP 2025",
         "doi": "10.18653/v1/2025.findings-emnlp.338",
-        "keywords": "exploitation, sparse-autoencoder, refusal-features, mechanistic-interpretability",
+        "keywords": "type:internal_safety, refusal, harmfulness, representation, sparse_autoencoder, refusal_direction, AdvBench, JailbreakBench, ASR, defense",
         "pages": "6377--6399",
         "publisher": "Association for Computational Linguistics",
         "title": "Understanding Refusal in Language Models with Sparse Autoencoders",
         "type": "inproceedings",
         "year": "2025"
     },
+
     "Jenny2026ActivationSurgery": {
         "abstract": "",
         "author": "Jenny, Mael and Dentan, Jeremie and Vanier, Sonia and Krajecki, Michael",
         "booktitle": "Proceedings of the 31st European Symposium on Research in Computer Security (ESORICS)",
         "doi": "10.48550/arXiv.2603.14278",
-        "keywords": "exploitation, activation-surgery, attack, white-box",
+        "keywords": "type:jailbreak_mechanisms, attack, white_box, activation_intervention, activation_surgery, AdvBench, ASR",
         "publisher": "Springer",
         "title": "Activation Surgery: Jailbreaking White-box LLMs without Touching the Prompt",
         "type": "inproceedings",
