0481aa6a16241214ae58ea7cb6f7943323444677
nikitaravojt <92792861+nikitaravojt@users.noreply.github.com>
2026-05-10 17:43:41 +0100

    Update bib.js

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,122 @@
-﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+const generatedBibEntries = {
+    "Yi2024JailbreakSurvey": {
+        "abstract": "",
+        "author": "Yi, Sibo and Liu, Yule and Sun, Zhen and Cong, Tianshuo and He, Xinlei and Song, Jiaxing and Xu, Ke and Li, Qi",
+        "doi": "10.48550/arXiv.2407.04295",
+        "journal": "arXiv preprint arXiv:2407.04295",
+        "keywords": "type:landscape, survey, taxonomy, attack, defense",
+        "title": "Jailbreak Attacks and Defenses Against Large Language Models: A Survey",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
+        "year": "2024"
+    },
+
+    "Zou2023Universal": {
+        "abstract": "",
+        "author": "Zou, Andy and Wang, Zifan and Carlini, Nicholas and Nasr, Milad and Kolter, J. Zico and Fredrikson, Matt",
+        "doi": "10.48550/arXiv.2307.15043",
+        "journal": "arXiv preprint arXiv:2307.15043",
+        "keywords": "type:landscape, attack, GCG, adversarial_suffix, gradient_based",
+        "title": "Universal and Transferable Adversarial Attacks on Aligned Language Models",
+        "type": "article",
+        "year": "2023"
+    },
+
+    "Chao2024JailbreakBench": {
+        "abstract": "",
+        "author": "Chao, Patrick and Debenedetti, Edoardo and Robey, Alexander and Andriushchenko, Maksym and Croce, Francesco and Sehwag, Vikash and Dobriban, Edgar and Flammarion, Nicolas and Pappas, George J. and Tramer, Florian and Hassani, Hamed and Wong, Eric",
+        "booktitle": "Advances in Neural Information Processing Systems",
+        "doi": "10.52202/079017-1745",
+        "keywords": "type:landscape, benchmark, evaluation, ASR",
+        "title": "JailbreakBench: An Open Robustness Benchmark for Jailbreaking Large Language Models",
+        "type": "inproceedings",
+        "volume": "37",
+        "year": "2024"
+    },
+
+    "Arditi2024Refusal": {
+        "abstract": "",
+        "author": "Arditi, Andy and Obeso, Oscar and Syed, Aaquib and Paleka, Daniel and Panickssery, Nina and Gurnee, Wes and Nanda, Neel",
+        "booktitle": "Advances in Neural Information Processing Systems",
+        "doi": "10.52202/079017-4322",
+        "keywords": "type:internal_safety, refusal_direction, activation_space, mechanistic_interpretability",
+        "pages": "136037--136083",
+        "title": "Refusal in Language Models Is Mediated by a Single Direction",
+        "type": "inproceedings",
+        "volume": "37",
+        "year": "2024"
+    },
+
+    "Zhao2025Harmfulness": {
+        "abstract": "",
+        "author": "Zhao, Jiachen and Huang, Jing and Wu, Zhengxuan and Bau, David and Shi, Weiyan",
+        "booktitle": "Advances in Neural Information Processing Systems",
+        "doi": "10.48550/arXiv.2507.11878",
+        "keywords": "type:internal_safety, harmfulness, refusal_direction, representation",
+        "title": "LLMs Encode Harmfulness and Refusal Separately",
+        "type": "inproceedings",
+        "volume": "38",
+        "year": "2025"
+    },
+
+    "Zhao2026SafetyNeurons": {
+        "abstract": "",
+        "author": "Zhao, Chongwen and Ke, Yutong and Huang, Kaizhu",
+        "booktitle": "Proceedings of the 19th Conference of the European Chapter of the Association for Computational Linguistics (Volume 1: Long Papers)",
+        "doi": "10.18653/v1/2026.eacl-long.83",
+        "keywords": "type:internal_safety, safety_neurons, MLP, defense",
+        "pages": "1889--1906",
+        "publisher": "Association for Computational Linguistics",
+        "title": "Unraveling LLM Jailbreaks Through Safety Knowledge Neurons",
+        "type": "inproceedings",
+        "year": "2026"
+    },
+
+    "Xu2024SCAV": {
+        "abstract": "",
+        "author": "Xu, Zhihao and Huang, Ruixuan and Chen, Changyu and Wang, Xiting",
+        "booktitle": "Advances in Neural Information Processing Systems",
+        "doi": "10.52202/079017-3706",
+        "keywords": "type:exploitation, concept_activation_vector, safety_boundary, attack",
+        "title": "Uncovering Safety Risks of Large Language Models through Concept Activation Vector",
+        "type": "inproceedings",
+        "volume": "37",
+        "year": "2024"
+    },
+
+    "Gao2025SafetyBoundaries": {
+        "abstract": "",
+        "author": "Gao, Lang and Geng, Jiahui and Zhang, Xiangliang and Nakov, Preslav and Chen, Xiuying",
+        "booktitle": "Proceedings of the 63rd Annual Meeting of the Association for Computational Linguistics (Volume 1: Long Papers)",
+        "doi": "10.18653/v1/2025.acl-long.1233",
+        "keywords": "type:exploitation, safety_boundary, activation_shift, defense",
+        "pages": "25378--25398",
+        "publisher": "Association for Computational Linguistics",
+        "title": "Shaping the Safety Boundaries: Understanding and Defending Against Jailbreaks in Large Language Models",
+        "type": "inproceedings",
+        "year": "2025"
+    },
+
+    "Yeo2025SAERefusal": {
+        "abstract": "",
+        "author": "Yeo, Wei Jie and Prakash, Nirmalendu and Neo, Clement and Satapathy, Ranjan and Lee, Roy Ka-Wei and Cambria, Erik",
+        "booktitle": "Findings of the Association for Computational Linguistics: EMNLP 2025",
+        "doi": "10.18653/v1/2025.findings-emnlp.338",
+        "keywords": "type:internal_safety, sparse_autoencoder, refusal_features, mechanistic_interpretability",
+        "pages": "6377--6399",
+        "publisher": "Association for Computational Linguistics",
+        "title": "Understanding Refusal in Language Models with Sparse Autoencoders",
+        "type": "inproceedings",
+        "year": "2025"
+    },
+
+    "Jenny2026ActivationSurgery": {
+        "abstract": "",
+        "author": "Jenny, Mael and Dentan, Jeremie and Vanier, Sonia and Krajecki, Michael",
+        "doi": "10.48550/arXiv.2603.14278",
+        "journal": "arXiv preprint arXiv:2603.14278",
+        "keywords": "type:exploitation, activation_surgery, attack, white_box",
+        "title": "Activation Surgery: Jailbreaking White-box LLMs without Touching the Prompt",
+        "type": "article",
+        "year": "2026"
     }
 };
